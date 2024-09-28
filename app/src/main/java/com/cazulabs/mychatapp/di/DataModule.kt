1ea9bd7cac1fd66afbe26e9c1c2f28d0f95227b1
Carlos <carloscga97@gmail.com>
2024-09-28 16:36:10 +0200

    add firebase library dependency injection

---
+++ app/src/main/java/com/cazulabs/mychatapp/di/DataModule.kt
@@ -0,0 +1,25 @@
+package com.cazulabs.mychatapp.di
+
+import com.cazulabs.mychatapp.data.network.FirebaseChatService
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabaseReference() = Firebase.database.reference
+
+    @Singleton
+    @Provides
+    fun provideFirebaseService(reference: DatabaseReference) = FirebaseChatService(reference)
+
+}
