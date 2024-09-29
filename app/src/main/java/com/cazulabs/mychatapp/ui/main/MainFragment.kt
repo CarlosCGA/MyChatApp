dede0501efd7cdb2c2c5e6edfa168382e98ab8fd
Carlos <carloscga97@gmail.com>
2024-09-29 22:42:19 +0200

    add state management to manage if user is registered

--- app/src/main/java/com/cazulabs/mychatapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/cazulabs/mychatapp/ui/main/MainFragment.kt
@@ -1,15 +1,18 @@
 package com.cazulabs.mychatapp.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.cazulabs.mychatapp.R
 import com.cazulabs.mychatapp.databinding.FragmentMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
@@ -23,13 +26,34 @@ class MainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         binding.btnChat.setOnClickListener {
-            if(!binding.tieUsername.text.isNullOrEmpty()) {
+            if (!binding.tieUsername.text.isNullOrEmpty()) {
                 viewModel.saveUsername(binding.tieUsername.text.toString())
                 findNavController().navigate(R.id.action_main_fragment_to_chat_fragment)
             }
         }
+        subscribeToState()
 
         return binding.root
+    }
+
+    private fun subscribeToState() {
+        lifecycleScope.launch {
+            viewModel.uiState.collect { state ->
+                when (state) {
+                    MainViewState.LOADING -> {
+                        binding.progressBar.isVisible = true
+                    }
+
+                    MainViewState.REGISTERED -> {
+                        findNavController().navigate(R.id.action_main_fragment_to_chat_fragment)
+                    }
+
+                    MainViewState.UNREGISTERED -> {
+                        binding.progressBar.isVisible = false
+                    }
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/cazulabs/mychatapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/cazulabs/mychatapp/ui/main/MainViewModel.kt
@@ -2,14 +2,37 @@ package com.cazulabs.mychatapp.ui.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.cazulabs.mychatapp.domain.GetUsernameUseCase
 import com.cazulabs.mychatapp.domain.SaveUsernameUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val saveUsernameUseCase: SaveUsernameUseCase) : ViewModel() {
+class MainViewModel @Inject constructor(
+    private val saveUsernameUseCase: SaveUsernameUseCase,
+    private val getUsernameUseCase: GetUsernameUseCase
+) : ViewModel() {
+
+    init {
+        verifyUserLogged()
+    }
+
+    private var _uiState = MutableStateFlow<MainViewState>(MainViewState.LOADING)
+    val uiState: StateFlow<MainViewState> = _uiState
+
+    private fun verifyUserLogged() {
+        viewModelScope.launch {
+            val username = getUsernameUseCase()
+            if (username.isNotEmpty())
+                _uiState.value = MainViewState.REGISTERED
+            else
+                _uiState.value = MainViewState.UNREGISTERED
+        }
+    }
 
     fun saveUsername(username: String) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -17,4 +40,10 @@ class MainViewModel @Inject constructor(private val saveUsernameUseCase: SaveUse
         }
     }
 
+}
+
+sealed class MainViewState {
+    object LOADING : MainViewState()
+    object UNREGISTERED : MainViewState()
+    object REGISTERED : MainViewState()
 }
